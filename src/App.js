0baf9c93a3354b582dc9df811b3f8bd92f571d94
geof <>
2021-01-07 15:09:12 +0300

    Started on header component, added icons and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+ 
+      {/* app body */}
+      {/* sidebar */}
+      {/* feed */}
+      {/* widgets */}
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAo4_QVWQuB5yi9AY_6_GZ628dWgeV4g_c",
+    authDomain: "fb-clone-29ac0.firebaseapp.com",
+    projectId: "fb-clone-29ac0",
+    storageBucket: "fb-clone-29ac0.appspot.com",
+    messagingSenderId: "31180417620",
+    appId: "1:31180417620:web:803e2530f3643768a05bc1"
+  };
+  // Initialize Firebase
+//   firebase.initializeApp(firebaseConfig);
