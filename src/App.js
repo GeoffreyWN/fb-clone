2d3727ad5c01adc818260e35fa2b0c3ef6321c05
geof <>
2021-01-07 16:32:56 +0300

    added sidebar and siderow component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './Header';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
- 
+    <div className="app">
+      <Header />
+      <div className="app__body">
+        <Sidebar />
+      </div>
+
       {/* app body */}
       {/* sidebar */}
       {/* feed */}
