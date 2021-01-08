d23b5a93d9bd42981e5ef14c91efe05f52e696c6
geof <>
2021-01-08 12:16:34 +0300

    Added login, widgets component

---
+++ src/Login.js
@@ -0,0 +1,20 @@
+import { Button } from "@material-ui/core";
+import React from "react";
+import "./Login.css";
+
+function Login() {
+  const signIn = () => {};
+  return (
+    <div className="login">
+      <div className="login__logo">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/0/05/Facebook_Logo_%282019%29.png"  alt="logo" />
+        <img src="https://www.logo.wine/a/logo/Facebook/Facebook-Logo.wine.svg" alt="logo" />
+      </div>
+      <Button type="submit" onClick={signIn}>
+        Sign In
+      </Button>
+    </div>
+  );
+}
+
+export default Login;
