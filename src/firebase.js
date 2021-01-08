a2e48ce7c83327025c308be51f24cbc1456c0e09
geof <>
2021-01-08 12:30:34 +0300

    installed firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,4 @@
+import firebase from 'firebase'
 const firebaseConfig = {
     apiKey: "AIzaSyAo4_QVWQuB5yi9AY_6_GZ628dWgeV4g_c",
     authDomain: "fb-clone-29ac0.firebaseapp.com",
@@ -7,4 +8,10 @@ const firebaseConfig = {
     appId: "1:31180417620:web:803e2530f3643768a05bc1"
   };
   // Initialize Firebase
-//   firebase.initializeApp(firebaseConfig);
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export { auth, provider};
+  export default db;
