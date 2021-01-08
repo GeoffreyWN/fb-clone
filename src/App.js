2cc115480087b43c65ccafec00006555dc9f2a07
geof <>
2021-01-08 13:11:54 +0300

    successfully logged in a user via google

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import Feed from "./Feed";
 import Widgets from "./Widgets";
 import Login from "./Login";
+import { useStateValue } from "./StateProvider";
+
 
 function App() {
-  const user = null;
+  const [{user}, dispatch] = useStateValue();
+
   return (
     <div className="app">
       {!user ? (
