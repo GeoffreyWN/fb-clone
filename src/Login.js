540a06a863076f2c1773f14bac35b2d90a54c881
geof <>
2021-01-08 13:01:49 +0300

    added a reducer, stateprovider

--- src/Login.js
+++ src/Login.js
@@ -1,14 +1,26 @@
 import { Button } from "@material-ui/core";
 import React from "react";
+import { auth, provider } from "./firebase";
 import "./Login.css";
 
 function Login() {
-  const signIn = () => {};
+  const signIn = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((result) => console.log(result))
+      .catch((error) => alert(error.message));
+  };
   return (
     <div className="login">
       <div className="login__logo">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/0/05/Facebook_Logo_%282019%29.png"  alt="logo" />
-        <img src="https://www.logo.wine/a/logo/Facebook/Facebook-Logo.wine.svg" alt="logo" />
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/0/05/Facebook_Logo_%282019%29.png"
+          alt="logo"
+        />
+        <img
+          src="https://www.logo.wine/a/logo/Facebook/Facebook-Logo.wine.svg"
+          alt="logo"
+        />
       </div>
       <Button type="submit" onClick={signIn}>
         Sign In
